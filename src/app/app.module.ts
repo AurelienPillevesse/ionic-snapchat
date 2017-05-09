be4b832875505afe9db4e83bd146b3e3d6fc2d44
Aurelio <aurelienpillevesse@hotmail.fr>
2017-05-09 15:22:54 +0200

    add camera preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Camera } from '@ionic-native/camera';
 import { Firebase } from '@ionic-native/firebase';
+import { CameraPreview } from '@ionic-native/camera-preview';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -34,6 +35,7 @@ import { Snap } from '../pages/snap/snap';
   ],
   providers: [
     Camera,
+    CameraPreview,
     Firebase,
     StatusBar,
     SplashScreen,
--- src/pages/snap/snap.ts
+++ src/pages/snap/snap.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Camera } from '@ionic-native/camera';
+import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview';
 
 /**
 * Generated class for the Snap page.
@@ -16,30 +17,50 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class Snap {
 
-    constructor(private camera: Camera, public navCtrl: NavController, public navParams: NavParams) {
+    constructor(private cameraPreview: CameraPreview, private camera: Camera, public navCtrl: NavController, public navParams: NavParams) {
     }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad Snap');
-    }
 
+        const cameraPreviewOpts: CameraPreviewOptions = {
+            x: 0,
+            y: 0,
+            width: window.screen.width,
+            height: window.screen.height,
+            camera: 'rear',
+            tapPhoto: true,
+            previewDrag: true,
+            toBack: true,
+            alpha: 1
+        };
 
-    takePicture() {
-        const options: CameraOptions = {
-            quality: 100,
-            destinationType: this.camera.DestinationType.DATA_URL,
-            encodingType: this.camera.EncodingType.JPEG,
-            mediaType: this.camera.MediaType.PICTURE
+        this.cameraPreview.startCamera(cameraPreviewOpts).then(
+            (res) => {
+                console.log(res)
+            },
+            (err) => {
+                console.log(err)
+            });
         }
 
-        this.camera.getPicture(options).then((imageData) => {
-            // imageData is either a base64 encoded string or a file URI
-            // If it's base64:
-            let base64Image = 'data:image/jpeg;base64,' + imageData;
-            alert(base64Image);
-        }, (err) => {
-            // Handle error
-        });
+
+        /*takePicture() {
+        const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
     }
+
+    this.camera.getPicture(options).then((imageData) => {
+    // imageData is either a base64 encoded string or a file URI
+    // If it's base64:
+    let base64Image = 'data:image/jpeg;base64,' + imageData;
+    alert(base64Image);
+}, (err) => {
+// Handle error
+});
+}*/
 
 }
