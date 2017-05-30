60465e9756c7cee21579b167073fc256c7f67e33
Pierre Pic <pipic1@dpt_info.iut.local>
2017-05-30 15:23:31 +0200

    add user infos

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -5,60 +5,74 @@ import {
   LoadingController,
   Loading,
   AlertController } from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthProvider } from '../../providers/auth-service/auth-service';
-import { EmailValidator } from '../../validators/email';
+  import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+  import { AuthProvider } from '../../providers/auth-service/auth-service';
+  import { AngularFireDatabase,FirebaseListObservable} from 'angularfire2/database';
+  import { User} from '../../model/user'
+  import { EmailValidator } from '../../validators/email';
 
-@IonicPage()
-@Component({
-  selector: 'page-sign-up',
-  templateUrl: 'sign-up.html',
-})
-export class SignUp {
-public signupForm: FormGroup;
- loading: Loading;
- constructor(public navCtrl: NavController, public authProvider: AuthProvider,
-   public formBuilder: FormBuilder, public loadingCtrl: LoadingController,
-   public alertCtrl: AlertController) {
+  @IonicPage()
+  @Component({
+    selector: 'page-sign-up',
+    templateUrl: 'sign-up.html',
+  })
+  export class SignUp {
+    public signupForm: FormGroup;
+    users: FirebaseListObservable<User[]>;
+    loading: Loading;
 
-     this.signupForm = formBuilder.group({
-       email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
-       password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
-     });
-   }
 
-  /**
-   * If the form is valid it will call the AuthData service to sign the user up password displaying a loading
-   *  component while the user waits.
-   *
-   * If the form is invalid it will just log the form value, feel free to handle that as you like.
-   */
-   signupUser(){
-   if (!this.signupForm.valid){
-     console.log(this.signupForm.value);
-   } else {
-     this.authProvider.signupUser(this.signupForm.value.email,
-         this.signupForm.value.password)
-     .then(() => {
-       this.loading.dismiss().then( () => {
-         this.navCtrl.setRoot('Snap');
-       });
-     }, (error) => {
-       this.loading.dismiss().then( () => {
-         let alert = this.alertCtrl.create({
-           message: error.message,
-           buttons: [
-             {
-               text: "Ok",
-               role: 'cancel'
-             }
-           ]
-         });
-         alert.present();
-       });
-     });
-     this.loading = this.loadingCtrl.create();
-     this.loading.present();
-   }
- }
-}
+
+    constructor(public navCtrl: NavController, public authProvider: AuthProvider,
+      public formBuilder: FormBuilder, public loadingCtrl: LoadingController,
+      public alertCtrl: AlertController, angularfire: AngularFireDatabase) {
+      this.users = angularfire.list('/users');
+      this.signupForm =  formBuilder.group({
+        email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+        name: ['', Validators.compose([Validators.required])],
+        lastname: ['', Validators.compose([Validators.required])],
+        password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+      });
+    }
+
+    signupUser(){
+      if (!this.signupForm.valid){
+        console.log(this.signupForm.value);
+      } else {
+        this.authProvider.signupUser(this.signupForm.value.email,
+          this.signupForm.value.password)
+        .then(() => {
+          this.loading.dismiss().then( () => {
+            this.navCtrl.setRoot('Snap');
+          });
+        }, (error) => {
+          this.loading.dismiss().then( () => {
+            let alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [
+              {
+                text: "Ok",
+                role: 'cancel'
+              }
+              ]
+            });
+            alert.present();
+          });
+        });
+        this.loading = this.loadingCtrl.create();
+        this.loading.present();
+        this.registerUser(this.signupForm.value.email,this.signupForm.value.name,this.signupForm.value.lastname);
+      }
+    }
+
+
+    registerUser(login:string,name:string,lastname:string){
+      this.users.push({
+        name: name,
+        lastname: lastname,
+        login: login,
+        score: 0,
+      });
+    }
+
+  }
