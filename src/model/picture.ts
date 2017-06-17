f69e0dc9c00051d0ae7c57682801352d071a8779
Aurelio <aurelienpillevesse@hotmail.fr>
2017-06-17 14:18:12 +0200

    update doc

--- src/model/picture.ts
+++ src/model/picture.ts
@@ -2,5 +2,8 @@
  * The Picture class
  */
 export class Picture {
+  /**
+      * Constructor of Picture
+      */
   constructor(public content: string, public from: string, public to: Array<string>) {}
 }
--- src/model/user.ts
+++ src/model/user.ts
@@ -2,5 +2,8 @@
  * The User class
  */
 export class User {
+  /**
+    * Constructor of User
+    */
   constructor(public name: string, public lastname: string, public login: string, public score: number) {}
 }
