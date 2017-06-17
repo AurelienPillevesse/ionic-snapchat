fa663f40e8a2551ae1a60d1403dbbee8bedefb3c
Aurelio <aurelienpillevesse@hotmail.fr>
2017-06-17 14:12:58 +0200

    update doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { AngularFireAuth } from "angularfire2/auth";
 import { Component } from "@angular/core";
 import { Platform } from "ionic-angular";
 
+/**
+ * The main component
+ */
 @Component({
   templateUrl: "app.html"
 })
@@ -20,6 +23,7 @@ export class MyApp {
         authObserver.unsubscribe();
       }
     });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
